c86eb722844e8bcc117021b773915ef0fb9a6f8a
Deef <suslovvit@yandex.ru>
2026-09-03 14:24:15 +0300

    small adjustments

--- internal/bot/start.go
+++ internal/bot/start.go
@@ -5,7 +5,7 @@ import (
 )
 
 func startBot(b *Bot, update *botapi.Update) {
-	text := "Бот зупущен!\nСписок команд:\n/rates - курсы всех отслеживаемых валют\n/rates ...(BTCUSDT) - курс конкретной валюты(например: BTCUSDT)\n/start_auto ...(5) - запуск авторассылки курсов валют (например: на 5 минут)\n/stop_auto - остановка авторассылки курсов валют,\n/add_symbol ... (BTCUSDT) -добавить валюту в отслеживаемые (например:BTCUSDT)\n/remove_symbol ... (BTCUSDT) - удалить валюту из отслеживаемых (например:BTCUSDT)"
+	text := "Бот запущен!\nСписок команд:\n/rates - курсы всех отслеживаемых валют\n/rates ...(BTCUSDT) - курс конкретной валюты(например: BTCUSDT)\n/start_auto ...(5) - запуск авторассылки курсов валют (например: на 5 минут)\n/stop_auto - остановка авторассылки курсов валют,\n/add_symbol ... (BTCUSDT) -добавить валюту в отслеживаемые (например:BTCUSDT)\n/remove_symbol ... (BTCUSDT) - удалить валюту из отслеживаемых (например:BTCUSDT)"
 	msg := botapi.NewMessage(update.Message.Chat.ID, text)
 	b.bot.Send(msg)
 }
